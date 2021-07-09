45f57b880a5863bd09348c506d554471894beacf
trigger <triggerhappy187@live.com>
2021-07-08 23:40:09 -0700

    add youtube to footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,4 @@
-import { GithubOutlined } from '@ant-design/icons';
+import { GithubOutlined, YoutubeOutlined } from '@ant-design/icons';
 import { DefaultFooter } from '@ant-design/pro-layout';
 export default () => {
   const defaultMessage = 'Metal Gear Online 2 PC. All rights reserved. This website is not associated with Konami or its affiliates. Special thanks to SaveMGO.';
@@ -10,6 +10,12 @@ export default () => {
           key: 'github',
           title: <GithubOutlined />,
           href: 'https://github.com/cipherxof/mgo2pc-frontend',
+          blankTarget: true,
+        },
+        {
+          key: 'youtube',
+          title: <YoutubeOutlined />,
+          href: 'https://www.youtube.com/channel/UC_9m-kGKK95xj-GvqUERrJw/videos',
           blankTarget: true,
         }
       ]}
